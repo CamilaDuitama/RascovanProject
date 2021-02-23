6edfba147c9cf54201e996b29aaf3311555a4175
Camila Duitama <camiladuitama@gmail.com>
2021-02-23 18:10:55 +0100

    change of directory

--- Scripts/run_kmtricks.sh
+++ Scripts/run_kmtricks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=15
 #SBATCH --mem=300G
 #SBATCH --j= run_kmtricks
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+kmtricks.py -v run --file $1 --run-dir $2 --count-abundance-min $3 --mode $4
 
-kmtricks.py -v run --file $1 --run-dir $2 --max-memory 300000 --nb-cores 10
