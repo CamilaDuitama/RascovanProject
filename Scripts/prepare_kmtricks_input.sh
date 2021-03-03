e190198da9bf738f60fda7d59117b61728b47666
Camila Duitama <camiladuitama@gmail.com>
2021-03-03 20:36:50 +0100

    prepare_kmtricks_input.sh correction

--- Scripts/prepare_kmtricks_input.sh
+++ Scripts/prepare_kmtricks_input.sh
@@ -3,7 +3,7 @@ while read -r line ;do
  	if [ -f $2$line"_2.fastq.gz" ]; then
                  echo $line : $2$line"_1.fastq.gz ;"$2$line"_2.fastq.gz"
          else
-                 echo $line : $2$line"_1.fastq.gz"
+                 echo $line : $2$line".fastq.gz"
          fi
  done<$1
 
--- Scripts/run_kmtricks.sh
+++ Scripts/run_kmtricks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=300G
+#SBATCH --mem=200G
 #SBATCH --j= run_kmtricks
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-kmtricks.py -v run --file $1 --run-dir $2 --count-abundance-min $3 --mode $4
+kmtricks.py -v run --file $1 --run-dir $2 --count-abundance-min $3 --mode $4 --recurrence-min $5
 
 
