b603d0cd822fdafdd7893552e92040c2a2f6f10a
Camila Duitama <camiladuitama@gmail.com>
2021-03-19 10:20:25 +0100

    select oral human aSGenomes in notebook

--- Scripts/get_SRA_data.sh
+++ Scripts/get_SRA_data.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-fastq-dump --split-3 -A $1 --gzip -O /pasteur/homes/cduitama/SRA_Downloads
+fastq-dump --split-3 -A $1 --gzip -O /pasteur/sonic/homes/cduitama/SRA_Downloads
 
 echo "Accession number  $1 was downloaded"	
